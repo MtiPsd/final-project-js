32dc9ae1a148a0f32edc959d46d26488af2f1c62
Mahdi Pasandideh <mtipsd.git@gmail.com>
2023-11-11 16:22:02 +0330

    Implemented :: all CRUD methods for services

--- services/tasksService.js
+++ services/tasksService.js
@@ -2,13 +2,63 @@ import { API_TODOS_ENDPOINT } from "../config/apiConfig.js";
 import { tasksApi } from "../interceptor/todosApi.js";
 
 async function getTasks() {
-  const todos = await tasksApi.get(API_TODOS_ENDPOINT);
-  console.log(todos);
+  try {
+    const todos = await todosApi.get(API_TODOS_ENDPOINT);
+    return todos;
+  } catch (error) {
+    console.error(
+      "Error getting tasks:",
+      error.response ? error.response.data : error.message,
+    );
+    throw error;
+  }
 }
 
 async function createTask(newTask) {
-  const todos = await tasksApi.post(API_TODOS_ENDPOINT, newTask);
-  console.log(todos);
+  try {
+    const createdTask = await todosApi.post(
+      API_TODOS_ENDPOINT,
+      newTask,
+    );
+    return createdTask;
+  } catch (error) {
+    console.error(
+      "Error creating task:",
+      error.response ? error.response.data : error.message,
+    );
+    throw error;
+  }
 }
 
-export { getTasks, createTask };
+async function updateTask(taskId, updatedTask) {
+  try {
+    const updatedTaskResponse = await todosApi.put(
+      `${API_TODOS_ENDPOINT}/${taskId}`,
+      updatedTask,
+    );
+    return updatedTaskResponse;
+  } catch (error) {
+    console.error(
+      "Error updating task:",
+      error.response ? error.response.data : error.message,
+    );
+    throw error;
+  }
+}
+
+async function deleteTask(taskId) {
+  try {
+    const deletedTaskResponse = await todosApi.delete(
+      `${API_TODOS_ENDPOINT}/${taskId}`,
+    );
+    return deletedTaskResponse;
+  } catch (error) {
+    console.error(
+      "Error deleting task:",
+      error.response ? error.response.data : error.message,
+    );
+    throw error;
+  }
+}
+
+export { getTasks, createTask, updateTask, deleteTask };
