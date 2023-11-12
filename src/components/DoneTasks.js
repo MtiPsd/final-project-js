844b94367ce440f31354f82a8b303b6ff7d23ca9
Mahdi Pasandideh <mtipsd.git@gmail.com>
2023-11-12 19:21:29 +0330

    Completed :: all necessary logic and UI  infractions

--- src/components/DoneTasks.js
+++ src/components/DoneTasks.js
@@ -1,4 +1,8 @@
-import { getDones } from "../services/tasksService.js";
+import {
+  deleteDone,
+  getDones,
+  undoDone,
+} from "../services/tasksService.js";
 import taskItem from "./TaskItem.js";
 
 const undoDoneBtn = document.getElementById("done__icon--undo");
@@ -30,3 +34,51 @@ function createDoneTasks(tasks) {
     );
   });
 }
+
+async function undoCompletedTask(e) {
+  const target = e.target;
+
+  if (target.matches("#done__icon--undo")) {
+    const taskId = target.parentElement.dataset.id;
+
+    try {
+      undoDone(taskId);
+
+      const completedTask = document.querySelector(
+        `[data-id="${taskId}"]`,
+      ).parentElement;
+
+      if (completedTask) {
+        completedTask.remove();
+      }
+    } catch (error) {
+      console.error("Error undo the task:", error.message);
+    }
+  }
+}
+
+function deleteCompletedTask(e) {
+  const target = e.target;
+
+  if (target.matches("#done__icon--delete")) {
+    const taskId = target.parentElement.dataset.id;
+
+    try {
+      deleteDone(taskId);
+
+      const completedTask = document.querySelector(
+        `[data-id="${taskId}"]`,
+      ).parentElement;
+
+      if (completedTask) {
+        completedTask.remove();
+      }
+    } catch (error) {
+      console.error("Error undo the task:", error.message);
+    }
+  }
+}
+
+////////////////// LISTENERS //////////////////
+doneContentList.addEventListener("click", undoCompletedTask);
+doneContentList.addEventListener("click", deleteCompletedTask);
